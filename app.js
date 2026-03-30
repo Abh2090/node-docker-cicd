3e7eba466f97d41bd7b4f8c6218e93d6c7b13d9b
root <root@DESKTOP-4PG3OOQ.localdomain>
2026-03-31 02:20:14 +0530

    kubernetes related update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-	res.end("🔥CI/CD Pipeline updated successfully 🚀");
+	res.end("🔥Kubernetes Rolling update successfully 🚀");
 }).listen(8080, () => {
 	console.log("Server running on port 8080");
 });
