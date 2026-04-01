52428b1b126f1ec8df1f75a4d4f0744c8361ad2e
root <root@DESKTOP-4PG3OOQ.localdomain>
2026-04-01 20:27:25 +0530

    app file changes done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-	throw new Error("App brokenizzzzzzzzzzzzzzzzzzzz!);
+	throw new Error("App brokenizzzzzzzzzzzzzzzzzzzwwwwz!);
 }).listen(8080, () => {
 	console.log("Server running on port 8080");
 });
